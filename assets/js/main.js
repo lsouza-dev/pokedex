9f9847f0a2f0be9ff4cf38037ab6830faf4f3c0d
Luiz Fabiano de Souza <luizfabianods04@gmail.com>
2025-02-23 23:21:53 -0300

    Tela inicial da pokedex criada: Buscando da API e criando os componentes em tela

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,77 @@
+const pokemonsContainer = document.querySelector('.pokemons');
 
-// const GetPokemons = async () => {
-//     var content = await fetch('https://pokeapi.co/api/v2/pokemon');
-//     var data = await content.json();
-//     var pokemons = data.results;
+class Pokemon {
+    constructor(id, name, image, types) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.types = types;
+    }
+}
 
-//     console.log(pokemons);
-//     return pokemons;
-// }
+const GetPokemons = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+    const data = await response.json();
+    return GetPokemonsInfo(data.results);
+};
 
-// const GetPokemonsAbilites = async (pokemons) => {
-//     var pokeHabilities = [];
-//     const data = await pokemons.map(p => {
-//         p.abilities.map(a => {
-//             pokeHabilities.push(a.ability);
-//         });
-//     });
-//     console.log(pokeHabilities);
-//     return pokeHabilities;
-// }
+const GetPokemonsInfo = async (pokemons) => {
+    let pokeList = [];
+    for (let i = 0; i < pokemons.length; i++) {
+        const response = await fetch(pokemons[i].url);
+        const data = await response.json();
 
-// const pokemons = GetPokemons();
+        const poke = new Pokemon(
+            data.id,
+            data.name,
+            data.sprites.other.dream_world.front_default,
+            data.types.map(a => a.type.name)
+        );
+        pokeList.push(poke);
+    }
+    return pokeList;
+};
 
-// const habilities = GetPokemonsAbilites(pokemons);
+const CreatePokemonCards = (pokemons) => {
+    pokemonsContainer.innerHTML = '';
+
+    pokemons.forEach(p => {
+        let card = document.createElement('li');
+        let number = document.createElement('span');
+        let name = document.createElement('span');
+        let detail = document.createElement('div');
+        let image = document.createElement('img');
+        let types = document.createElement('ol');
+
+        card.classList.add('pokemon');
+        number.classList.add('number');
+        name.classList.add('name');
+        detail.classList.add("detail");
+        types.classList.add('types');
+
+        p.types.forEach(typeName => {
+            let type = document.createElement('li');
+            type.classList.add('type');
+            type.textContent = typeName;
+            types.appendChild(type);
+        });
+
+        number.textContent = `#${p.id}`;
+        name.textContent = p.name;
+        image.src = p.image;
+        image.alt = p.name;
+
+        card.appendChild(number);
+        card.appendChild(name);
+        card.appendChild(detail);
+        detail.appendChild(types);
+        detail.appendChild(image);
+        pokemonsContainer.appendChild(card);
+    });
+};
+
+// Chamando as funções corretamente
+(async () => {
+    const pokemons = await GetPokemons();
+    CreatePokemonCards(pokemons);
+})();
